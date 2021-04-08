141d8d684e2493a64028c79e9ae519be5966a4eb
Queen-Vaibhavi <77841320+Queen-Vaibhavi@users.noreply.github.com>
2021-04-08 14:07:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,7 +36,7 @@ function setup() {
 	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, starBody);
 	
-	starBody_options = {
+	star_options = {
 restitution: 1
   }
 	
